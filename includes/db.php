e2b4d24f758cf247a85d3d6426594737e1afa4d1
Pedro Campos <1240773@isep.ipp.pt>
2026-05-27 22:52:59 +0100

    Criação ligação a base de dados - back end

---
+++ includes/db.php
@@ -0,0 +1,20 @@
+<?php include 'includes/db.php'
+
+$host = '127.0.0.1';
+$db_name = 'Projeto_SIBDAS';
+$username = 'root';
+$password = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try { 
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
